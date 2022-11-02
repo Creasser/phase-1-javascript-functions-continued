73f82af55205a607be8eb264205982db25be7d01
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-01 22:02:51 -0500

    commit tree a40fa6fb8a84f509eb8b8363a99bd163fcf80ddc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (thing = 'go to the office'){
+    return `This Monday, I will ${thing}.`
+}
+
+function wrapAdjective(symbol = '*'){
+    return function (para = 'special'){
+        return `You are ${symbol}${para}${symbol}!`
+    }
+}
